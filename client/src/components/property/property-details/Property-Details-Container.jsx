255a69b1c8c4bfc7c1b5acfcac71cb81f5f33f59
johanna-konova <j.konova@abv.bg>
2025-03-21 12:21:44 +0200

    Wheelchair tire spinner and user actions for edit and delete added to property details container components

--- client/src/components/property/property-details/Property-Details-Container.jsx
+++ client/src/components/property/property-details/Property-Details-Container.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 
 import { usePropertyContext } from '../../../contexts/Property-Context';
+import { useAuthContext } from '../../../contexts/Auth-Context';
 
 import PropertyDetailsImages from './Property-Details-Images';
+import UserActions from '../../user-actions/User-Actions';
 import PropertyDetails from './Property-Details';
+import WheelchairTireSpinner from '../../loaders/Wheelcheir-Tire-Spinner';
 
 export default function PropertyDetailsContainer() {
-    const { propertyData, propertyFacilities } = usePropertyContext();
+    const { propertyData, propertyFacilities, isPropertyDataLoaded } = usePropertyContext();
+    const { id } = useAuthContext();
 
     return (
         <>
             <Container className="mt-5">
-                {propertyData.imageUrls && <PropertyDetailsImages imageUrls={propertyData.imageUrls} />}
+                {isPropertyDataLoaded
+                    ? <>
+                        {propertyData.imageUrls && <PropertyDetailsImages imageUrls={propertyData.imageUrls} />}
 
-                <PropertyDetails {...propertyData} facilities={propertyFacilities} />
+                        {(id === propertyData._ownerId) && <UserActions _id={propertyData._id} name={propertyData.name} />}
+
+                        <PropertyDetails {...propertyData} facilities={propertyFacilities} />
+                    </>
+                    : <WheelchairTireSpinner />
+                }
+
             </Container>
         </>
     )
