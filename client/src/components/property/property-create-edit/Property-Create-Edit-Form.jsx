3f400fef0becaae5e60080ec4316dae0216f8162
johanna-konova <j.konova@abv.bg>
2025-03-23 15:50:22 +0200

    Construct property data for editing sepparated in util

--- client/src/components/property/property-create-edit/Property-Create-Edit-Form.jsx
+++ client/src/components/property/property-create-edit/Property-Create-Edit-Form.jsx
@@ -10,6 +10,7 @@ import { usePropertyContext } from '../../../contexts/Property-Context';
 import { useBasicGetFetch } from '../../../hooks/use-basic-get-fetch';
 import * as propertyAPI from '../../../services/propertiesServices';
 import { propertySchema } from '../../../validations';
+import { constructPropertyDataForEditing } from '../../../utils/property-utils';
 
 import PropertyCreateEditFormStepOne from './Property-Create-Edit-Form-Step-One';
 import PropertyCreateEditFormStepTwo from './Property-Create-Edit-Form-Step-Two';
@@ -38,34 +39,11 @@ export default function PropertyCreateEditForm() {
         },
         mode: 'onChange',
         resolver: yupResolver(propertySchema),
-        context: { step },
     });
 
     useEffect(() => {
         if (propertyData._id !== undefined) {
-            reset({
-                "id": propertyData._id,
-                "step-1": {
-                    name: propertyData.name,
-                    typeId: propertyData.typeId,
-                    starsCount: propertyData.starsCount,
-                    checkIn: propertyData.checkIn,
-                    checkOut: propertyData.checkOut,
-                    address: propertyData.address,
-                    description: propertyData.description,
-                },
-                "step-2": {
-                    commonFacilityIds: propertyFacilities
-                        .filter(pf => pf.isForAccessibility === false)
-                        .map(pf => pf.facilityId),
-                    accessibilityIds: propertyFacilities
-                        .filter(pf => pf.isForAccessibility)
-                        .map(pf => pf.facilityId),
-                },
-                "step-3": {
-                    imageUrls: propertyData.imageUrls?.map((iu, i) => ({ id: i + 1, url: iu })),
-                }
-            });
+            reset(constructPropertyDataForEditing(propertyData, propertyFacilities));
         }
     }, [propertyData]);
 
--- client/src/utils/property-utils.jsx
+++ client/src/utils/property-utils.jsx
@@ -4,3 +4,29 @@ export const generateStarIcons = (starsCount) => new Array(starsCount)
 
 export const getCountryName = (address) =>
     address.substring(address.lastIndexOf(",") + 1).trim();
+
+export const constructPropertyDataForEditing = (propertyData, propertyFacilities) => {
+    return {
+        "id": propertyData._id,
+        "step-1": {
+            name: propertyData.name,
+            typeId: propertyData.typeId,
+            starsCount: propertyData.starsCount,
+            checkIn: propertyData.checkIn,
+            checkOut: propertyData.checkOut,
+            address: propertyData.address,
+            description: propertyData.description,
+        },
+        "step-2": {
+            commonFacilityIds: propertyFacilities
+                .filter(pf => pf.isForAccessibility === false)
+                .map(pf => pf.facilityId),
+            accessibilityIds: propertyFacilities
+                .filter(pf => pf.isForAccessibility)
+                .map(pf => pf.facilityId),
+        },
+        "step-3": {
+            imageUrls: propertyData.imageUrls?.map((iu, i) => ({ id: i + 1, url: iu })),
+        }
+    };
+}
