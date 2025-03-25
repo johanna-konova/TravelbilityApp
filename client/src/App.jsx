0272a464ad4d171e6d4e82d733646f6bf30d945f
johanna-konova <j.konova@abv.bg>
2025-03-25 11:54:47 +0200

    Our mission component / page changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,14 +11,14 @@ import Home from "./components/home/Home";
 import LoginForm from "./components/auth-forms/Login-Form";
 import RegisterForm from "./components/auth-forms/Register-Form";
 import AllProperties from "./components/properties/all-properties/All-Properties";
-import OurMission from "./components/Our-Mission";
+import OurMission from "./components/our-mission/Our-Mission";
 import PropertyDetailsContainer from "./components/property/property-details/Property-Details-Container";
 import UserProperties from "./components/properties/user-properties/User-Properties";
 import PropertyCreateEditForm from "./components/property/property-create-edit/Property-Create-Edit-Form";
 import Footer from "./components/Footer";
 import BackToTop from "./components/Back-To-Top";
 import PublisherGuard from "./components/common/Publisher-Guard";
-import NotFound from "./not-found/Not-Found";
+import NotFound from "./components/not-found/Not-Found";
 
 function App() {
     return (
